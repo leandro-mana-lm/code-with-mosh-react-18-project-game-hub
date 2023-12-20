0228321d2c4f325a5bf6d9c3fcc8cb19c460ef34
Leandro Mana <leandro.mana.lm@gmail.com>
2023-12-20 00:39:42 -0300

    lesson 06 | building the navigation bar.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { Grid, GridItem, Show } from '@chakra-ui/react';
 
+import NavBar from './components/NavBar';
+
 const App = () => {
   return (
     <Grid
@@ -8,8 +10,8 @@ const App = () => {
         lg: `'nav nav' 'aside main'`,
       }}
     >
-      <GridItem area='nav' bg='coral'>
-        Nav
+      <GridItem area='nav'>
+        <NavBar />
       </GridItem>
 
       <Show above='lg'>

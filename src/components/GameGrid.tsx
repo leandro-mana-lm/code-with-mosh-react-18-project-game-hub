d37c7ca0df79624ed0176695c00235c74613a9b2
Leandro Mana <leandro.mana.lm@gmail.com>
2023-12-23 22:42:11 -0300

    lesson 15 | improving user experience with loading skeletons.

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,10 +1,13 @@
 import { SimpleGrid, Text } from '@chakra-ui/react';
 
 import GameCard from './GameCard';
+import GameCardSkeleton from './GameCardSkeleton';
 import useGames from '../hooks/useGames';
 
 const GameGrid = () => {
-  const { games, error } = useGames();
+  const { games, error, isLoading } = useGames();
+
+  const skeletons = [1, 2, 3, 4, 5, 6];
 
   return (
     <>
@@ -19,6 +22,9 @@ const GameGrid = () => {
         }}
         spacing={10}
       >
+        {isLoading &&
+          skeletons.map((skeleton) => <GameCardSkeleton key={skeleton} />)}
+
         {games.map((game) => (
           <GameCard key={game.id} game={game} />
         ))}
